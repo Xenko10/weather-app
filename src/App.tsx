25500923d2c5caf4b0b7d291b19b19272600800d
Xenko10 <kubinios_2002@o2.pl>
2023-09-16 11:04:10 +0200

    use proper command to get acces of env variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_KEY = import.meta.env.VITE_API_KEY;
   console.log(API_KEY);
   return <div className='display'>{API_KEY}</div>;
 }

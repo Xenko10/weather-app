78c524b7adfead2deb37f14355445dabc50c6044
Xenko10 <kubinios_2002@o2.pl>
2023-09-16 12:29:30 +0200

    add api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import axios from "axios";
+const API_KEY: string = import.meta.env.VITE_API_KEY;
+
 function App() {
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  console.log(API_KEY);
+  const city_name = "Warszawa";
+  try {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  } catch {
+    console.log("Error");
+  }
   return <div className='display'>{API_KEY}</div>;
 }
 

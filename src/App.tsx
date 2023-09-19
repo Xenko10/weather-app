0bf63ea6c61b2fbfdafc5d7ea5a42596a490eb4a
Xenko10 <kubinios_2002@o2.pl>
2023-09-19 09:27:30 +0200

    show humidity and windspeed info in info component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ function App() {
         city={weatherData.data?.name || ""}
         error={weatherData.error}
         temperature={weatherData.data?.main.temp - 273.15}
+        humidity={weatherData.data?.main.humidity}
+        windSpeed={weatherData.data?.wind.speed}
       />
     </div>
   );
--- src/Info.tsx
+++ src/Info.tsx
@@ -2,13 +2,23 @@ interface InfoProps {
   city: string;
   error: string;
   temperature: number;
+  humidity: number;
+  windSpeed: number;
 }
 
-const Info: React.FC<InfoProps> = ({ temperature, city, error }) => {
+const Info: React.FC<InfoProps> = ({
+  temperature,
+  city,
+  error,
+  humidity,
+  windSpeed,
+}) => {
   return (
     <>
       <div>{temperature ? Math.round(temperature) + " ℃" : error}</div>
       <div>{city}</div>
+      <div>{humidity ? humidity + "%" : ""}</div>
+      <div>{windSpeed ? Math.round(windSpeed * 3.6) + " km/h " : ""}</div>
     </>
   );
 };

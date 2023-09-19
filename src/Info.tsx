c112ca8c030cf7235f7a0725013e7cca1a3ce9aa
Xenko10 <kubinios_2002@o2.pl>
2023-09-19 09:44:29 +0200

    show icon when (city) input is correct

--- src/Info.tsx
+++ src/Info.tsx
@@ -1,3 +1,5 @@
+import styles from "../public/Info.module.css";
+
 interface InfoProps {
   city: string;
   error: string;
@@ -14,12 +16,13 @@ const Info: React.FC<InfoProps> = ({
   windSpeed,
 }) => {
   return (
-    <>
+    <div className={styles.info}>
+      {city && <img src='./src/assets/sunny-icon.png' alt='sunny icon' />}
       <div>{temperature ? Math.round(temperature) + " ℃" : error}</div>
       <div>{city}</div>
       <div>{humidity ? humidity + "%" : ""}</div>
       <div>{windSpeed ? Math.round(windSpeed * 3.6) + " km/h " : ""}</div>
-    </>
+    </div>
   );
 };
 
